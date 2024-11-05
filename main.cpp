4f71512b8a771be8343aafe1b02fce2b2da1d1f5
Ryan Khieu <159482357+RyanKhieu@users.noreply.github.com>
2024-11-05 08:24:06 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Name: Ryan Khieu
+// Program Name: Loop Lab
+// Date: 11/5/24
+// Extra: 
+
+#include <iostream>
+#include <string>
+
+std::string days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+int main() {
+    for (int i = 0; i < 7; i++) {
+        std::cout << days[i] << std::endl;
+    }
+}
